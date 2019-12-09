1d92485225cfd207e811da71b8e0c2e65cf28904
Dinesh Rajput <dineshrajput@ucreate.co.in>
2019-12-09 22:26:41 +0530

    Removed auto gen code and added map screen

---
+++ src/screens/MapScreen/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class index extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Map screen </Text>
+      </View>
+    );
+  }
+}
---
+++ src/screens/MapScreen/style.js
@@ -0,0 +1,9 @@
+import {StyleSheet} from 'react-native';
+
+const Style = StyleSheet.create({
+  parentContainer: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+});
+export default Style;
